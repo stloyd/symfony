ccc378501ef330b28cec2f719a58dac9a4c8d3cb
Joseph Bielawski <stloyd@gmail.com>
2013-05-28 09:04:49 +0200

    [Console] Extend TableHelper test with style tags

--- src/Symfony/Component/Console/Tests/Helper/TableHelperTest.php
+++ src/Symfony/Component/Console/Tests/Helper/TableHelperTest.php
@@ -171,6 +171,27 @@ TABLE
                 TableHelper::LAYOUT_DEFAULT,
                 '',
             ),
+            array(
+                array('ISBN', '<error>Title</error>'),
+                array(
+                    array('<error>99921-58-10-7</error>', 'Divine Comedy', 'Dante Alighieri'),
+                    array('9971-5-0210-0'),
+                    array('960-425-059-0', '<error>The Lord of the Rings</error>', 'J. R. R. Tolkien'),
+                    array('80-902734-1-6', 'And Then There Were None', '<error>Agatha Christie</error>'),
+                ),
+                TableHelper::LAYOUT_DEFAULT,
+<<<TABLE
++---------------+--------------------------+------------------+
+| ISBN          | Title                    |                  |
++---------------+--------------------------+------------------+
+| 99921-58-10-7 | Divine Comedy            | Dante Alighieri  |
+| 9971-5-0210-0 |                          |                  |
+| 960-425-059-0 | The Lord of the Rings    | J. R. R. Tolkien |
+| 80-902734-1-6 | And Then There Were None | Agatha Christie  |
++---------------+--------------------------+------------------+
+
+TABLE
+            ),
         );
     }
 
